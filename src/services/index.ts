f00194ebf28f8964bc49c78a5e58b18a98add21e
kumkum-thakur <chaudharykumkum148@gmail.com>
2026-01-31 21:51:03 +0530

    Agent 1: Auto-sync 31-01-2026 21:51:03.37

---
+++ src/services/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Service layer exports
+ * 
+ * Centralized export point for all services
+ */
+
+export { persistenceService, PersistenceServiceImpl, PersistenceError } from './persistenceService';
