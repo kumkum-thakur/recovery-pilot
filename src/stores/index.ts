901b06aaa782986775efc48374e2545b937a6202
kumkum-thakur <chaudharykumkum148@gmail.com>
2026-01-31 22:05:42 +0530

    Agent 1: Auto-sync 31-01-2026 22:05:42.37

---
+++ src/stores/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Stores index - Central export for all Zustand stores
+ * 
+ * Exports:
+ * - useUserStore: User authentication and profile management
+ */
+
+export { useUserStore } from './userStore';
