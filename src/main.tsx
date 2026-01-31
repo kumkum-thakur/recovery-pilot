fbb061457735a94eaa53bfa7bcb643e9dfe480f8
kumkum-thakur <chaudharykumkum148@gmail.com>
2026-01-31 22:44:55 +0530

    Agent 1: Auto-sync 31-01-2026 22:44:55.33

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { persistenceService } from './services/persistenceService'
+import { initializeSeedData } from './services/seedData'
+
+// Initialize seed data on app startup
+initializeSeedData(persistenceService);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
